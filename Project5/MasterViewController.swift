32766661736ae1c858f9dd783ef5f6fdfd12295e
Robert McBryde <robert.mcbryde@kotikan.com>
2015-10-02 22:54:38 +0100

    Creation of word array from start.txt file and reading of it via MainBundle and convenience String method. Start the game by randomly shuffling the array of words and setting the title of the ViewController to be the word for the user to work from.

--- Project5/MasterViewController.swift
+++ Project5/MasterViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import GameKit
 
 class MasterViewController: UITableViewController {
     
-    var objects = [AnyObject]()
+    var objects = [String]()
+    var allWords = [String]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+            } else {
+                allWords = ["silkworm"]
+            }
+        }
+        startGame()
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -26,6 +36,16 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - Game Logic
+
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
+    }
+    
     
     // MARK: - Segues
 
@@ -43,8 +63,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
